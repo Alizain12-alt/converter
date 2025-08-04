f96ab0262d1a8fdbc68956bf3239fe21381bbd15
Alizain12-alt <syedalizain05121472@gmail.com>
2025-08-04 16:05:09 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:window_size/window_size.dart';
-
-import 'common/settings.dart';
 import 'common/convert_pipe.dart';
 import 'widgets/cconverter.dart';
 
